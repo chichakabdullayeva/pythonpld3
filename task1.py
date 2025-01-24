7835f2024554e4db058d64903b5dbd1dd0f0e05e
root <cicekabdullayeva28@gmail.com>
2025-01-24 21:37:24 +0000

    done

---
+++ task1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+y = int(input())
+if y>0 :
+    print (y,"is positive")
+elif y<0 :
+    print (y,"is negative")
+else:
+    print (y,"is zero")
---
+++ task2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+y = str(input('enter new string\n'))
+length = 0
+for i in y :
+    length = length +1
+print (length)
