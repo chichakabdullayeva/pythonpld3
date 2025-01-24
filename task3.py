cfa283dd90f27e90ec4506814d7d7a7020ce8583
root <cicekabdullayeva28@gmail.com>
2025-01-24 21:47:42 +0000

    done

---
+++ task3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+y = str(input())
+length = 0
+for i in y :
+    length = length + 1
+print (y[length-1])
---
+++ task4.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+number = int(input())
+if number == 0:
+    print (number,"is not even or odd")
+else:
+    if number % 2 == 0:
+        print (number,"is even")
+    else :
+        print (number,"is odd")
